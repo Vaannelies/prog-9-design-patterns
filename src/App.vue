f535946b0d6754c92bb5a7f170eb8f705b0814f1
Annelies <0962949@hr.nl>
2021-02-16 17:30:10 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <Field/> -->
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import Field from './components/Field'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // Field,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Field.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p class='text'>hoi</p>
+    </div>
+</template>
+
+<script>
+export default class Field {
+    
+}
+</script>
+<style scoped>
+    .text {
+        color: black;
+    }
+</style>
