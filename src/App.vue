aaf2f19e074faed5829f3b7347ea964d4aa3b9f5
Annelies <0962949@hr.nl>
2021-02-17 15:12:48 +0100

    Converted to typescript project

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,30 @@
 <template>
-  <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <p class="title">To do</p>
-    <to-do-list/>
-    <!-- <HelloWorld/> -->
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
 
-<script lang='ts'>
-import { Component } from 'vue-property-decorator'
-import Vue from 'Vue'
-import ToDoList from './components/ToDoList';
-
-@Component({
-   components: {
-     ToDoList,
-   }
-})
-export default class App extends Vue {
-
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
 
-<style lang='ts'>
-  @import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+#nav {
+  padding: 30px;
 
-  body {
-    margin: 0;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
-  #app {
-    /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale; */
-    /* text-align: center; */
-    /* color: #2c3e50; */
-    display:flex;
-    height: 100vh;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    /* margin-top: 60px; */
-    background-color: white;
-
-    /* #177E89 */
-    /* #084C61 */
-    /* #DB3A34 */
-    /* #FFC857 */
-    /* #323031 */
-
-
-    /* #eff7cf */
-    /* #bad9b5 */
-    /* #aba361 */
-    /* #732c2c */
-    /* #420c14 */
-  }
-
-  .title {
-    font-family: 'Varela Round';
-    font-size: 40px;
-    font-weight: 1;
-    margin-top: 0;
-    color: #DB3A34;
-    -webkit-text-stroke: 2px #FFC857;
-  }
+}
 </style>
